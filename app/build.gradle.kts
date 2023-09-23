68e348c40b327df3ff87ed8f92c2ad664099f137
SonicVelikiy <weather_moonlight_1996@mail.ru>
2023-09-23 17:45:28 +0500

    driver profile design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
     implementation("com.google.firebase:firebase-analytics-ktx:21.3.0")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -62,5 +63,8 @@ dependencies {
 
     //sms input layout...
     implementation("com.github.mukeshsolanki:android-otpview-pinview:2.1.2")
+
+    //circleImageView...
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
 
 }
